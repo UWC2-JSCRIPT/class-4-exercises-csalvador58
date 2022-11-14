166494b69507e57446e0c914f167b2a2e5b79b9d
Chris Salvador <csalvador58@gmail.com>
2022-11-14 15:00:21 -0800

    Updated logic in if else code

--- foodIsCooked.js
+++ foodIsCooked.js
@@ -6,19 +6,19 @@
  * @returns {boolean} isCooked
  */
 
-const foodIsCooked = function(kind, internalTemp, doneness) {
+const foodIsCooked = (kind, internalTemp, doneness) => {
   // Check meat type
   if (kind == 'chicken') {
-    // Check if meat temp is within required range
+    // Check if meat temp is above required limit
     return internalTemp > 165 ? true : false;
 
   // Check meat type
   } else if (kind == 'beef') {
-    // Check doneness then check if meat temp within required range
+    // Check doneness then check if meat temp is above required limit
     if (doneness == 'rare') {
-      return (internalTemp > 125 && internalTemp <= 135) ? true : false;
+      return (internalTemp > 125) ? true : false;
     } else if (doneness == 'medium') {
-        return (internalTemp > 135 && internalTemp <= 155) ? true : false;
+        return (internalTemp > 135) ? true : false;
     } else if (doneness == 'well') {
         return (internalTemp > 155) ? true : false;
     }
@@ -32,4 +32,4 @@ console.log(foodIsCooked('chicken', 90)); // should be false
 console.log(foodIsCooked('chicken', 190)); // should be true
 console.log(foodIsCooked('beef', 138, 'well')); // should be false
 console.log(foodIsCooked('beef', 138, 'medium')); // should be true
-console.log(foodIsCooked('beef', 138, 'rare')); // should be false
+console.log(foodIsCooked('beef', 138, 'rare')); // should be true
