ab441e6e3e70b9c58c47b912e8fe0184e6bf49f9
Chris Salvador <csalvador58@gmail.com>
2022-11-15 11:55:35 -0800

    Working on blackjack game

--- blackjack.js
+++ blackjack.js
@@ -5,11 +5,20 @@ const blackjackDeck = getDeck();
  * @constructor
  * @param {string} name - The name of the player
  */
-class CardPlayer {}; //TODO
+class CardPlayer {
+  constructor(name) {
+    this.name = name;
+    this.hand = [];
+  }
+  drawCard() {
+    const drawRandomCard = blackjackDeck[Math.floor(Math.random() * 52)];
+    this.hand.push(drawRandomCard);
+  }
+}; //TODO
 
 // CREATE TWO NEW CardPlayers
-const dealer; // TODO
-const player; // TODO
+const dealer = new CardPlayer('Dealer Dan'); // TODO
+const player = new CardPlayer('Player Penelope'); // TODO
 
 /**
  * Calculates the score of a Blackjack hand
@@ -19,8 +28,46 @@ const player; // TODO
  * @returns {boolean} blackJackScore.isSoft
  */
 const calcPoints = (hand) => {
-  // CREATE FUNCTION HERE
+  // Filter for only card val items in array using map method
+  const cardValues = hand.map(item => item.val);
+  // console.log(cardValues)
 
+  // Calculate total points using reduce method on cardValues array
+  const totalPoints = cardValues.reduce((sum, item) => sum + item);
+
+  // Filter for Aces in hand array using filter method
+  const numberOfAces = hand.filter(item => item.displayVal == 'Ace');
+  // console.log(numberOfAces.length)
+
+
+  // Using if else statements to cycle through different hands with zero to multiple Aces
+  if (numberOfAces.length == 0) {               // No aces, return points and hand is not soft
+    return {total: totalPoints, isSoft: false} 
+  } else if (numberOfAces.length == 1) {               // 1 ace check
+      if (totalPoints > 21) {
+        return {total: totalPoints - 10, isSoft: false}
+      } else {
+        return {total: totalPoints, isSoft: true}
+      }
+  } else if (numberOfAces. length == 2) {               // 2 aces check
+      if ((totalPoints - 10) > 21) {
+        return {total: totalPoints - 20, isSoft: false}
+      } else {
+        return {total: totalPoints - 10, isSoft: true}
+      }
+  } else if (numberOfAces.length == 3) {               // 3 aces check
+      if ((totalPoints - 20) > 21) {
+        return {total: totalPoints - 30, isSoft: false}
+      } else {
+        return {total: totalPoints - 20, isSoft: true}
+      }
+  } else if (numberOfAces.length == 4) {              // 4 aces check
+      if ((totalPoints - 30) > 21) {
+        return {total: totalPoints - 40, isSoft: false}
+      } else {
+        return {total: totalPoints - 30, isSoft: true}
+      }
+  }
 }
 
 /**
